b43649e433b73543a36d7cf25d9d71516040bdba
Branislav Stojanovic <banestojanovics@gmail.com>
2025-02-14 12:12:40 +0100

    fixing some issues

--- app/Data/ProductData.php
+++ app/Data/ProductData.php
@@ -8,68 +8,62 @@ use Illuminate\Database\Eloquent\Collection;
 use Spatie\LaravelData\Attributes\Computed;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Optional;
+use Spatie\LaravelData\DataCollection;
 use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class ProductData extends Data
 {
     #[Computed]
-    public \Illuminate\Support\Collection|array|null $grouped_variations;
+    public \Illuminate\Support\Collection|array|null $grouped_variations = null;
 
     #[Computed]
-    public \Illuminate\Support\Collection|array|null $grouped_attributes;
+    public \Illuminate\Support\Collection|array|null $grouped_attributes = null;
 
     #[Computed]
-    public ?array $additional;
+    public ?array $additional = null;
 
     #[Computed]
-    public ?CategoryData $category;
+    public ?CategoryData $category = null;
 
     #[Computed]
-    public ?TagData $tag;
+    public ?TagData $tag = null;
 
     public function __construct(
         public int $id,
         public string $name,
         public string $slug,
         public string $sku,
-        public float|Optional $price,
-        public float|Optional $base_price,
-        public int|Optional $stock,
+        public ?float $price,
+        public ?float $base_price,
+        public ?int $stock,
         public ProductStockStatus $stock_status,
-        public string|Optional $description,
-        public ProductStatus|Optional $status,
-        public array|Optional $data,
-        public Media|Optional $cover,
-        public MediaCollection|Optional $photos,
+        public ?string $description,
+        public ?ProductStatus $status,
+        public ?array $data,
+        public ?Media $cover,
+        public ?MediaCollection $photos,
         #[DataCollectionOf(ProductVariationData::class)]
-        public Collection|Optional $variations,
+        public ?Collection $variations,
         #[DataCollectionOf(AttributeValueData::class)]
-        public Collection|Optional $attributes,
+        public ?Collection $attributes,
         #[DataCollectionOf(CategoryData::class)]
-        public Collection|Optional $categories,
-        public DiscountData|Optional $discount,
+        public ?Collection $categories,
+        public ?DiscountData $discount,
         #[DataCollectionOf(DiscountData::class)]
-        public Collection|Optional $discounts,
+        public ?Collection $discounts,
         #[DataCollectionOf(TagData::class)]
-        public Collection|Optional $productTags,
+        public ?Collection $productTags,
     ) {
-        $this->grouped_variations = $variations->flatMap(fn ($variation) => $variation->variations)->groupBy('variation.name') ?? [];
-        if ($this->attributes instanceof Collection) {
-            $this->grouped_attributes = $this->attributes->groupBy('attribute.name');
-        }
+        $this->grouped_variations = $this->variations?->flatMap(fn ($variation) => $variation->variations)->groupBy('variation.name') ?? [];
+        $this->grouped_attributes = $this->attributes?->groupBy('attribute.name') ?? [];
 
         $this->additional = [
             __('Sastojci') => $data['ingredients'] ?? '',
             __('Najbolje upotrebiti do') => $data['valid_until'] ?? '',
         ];
 
-        if ($this->categories instanceof Collection) {
-            $this->category = $this->categories->last();
-        }
-        if ($this->productTags instanceof Collection) {
-            $this->tag = $this->productTags->first();
-        }
+        $this->category = $categories?->last();
+        $this->tag = $this->productTags?->first();
     }
 }
